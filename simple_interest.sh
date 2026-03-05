f8fe5fe74c8b36c490e9b14175707591c9b79e4f
Chiquinquira Maldonado <sarielkysmp@gmail.com>
2026-03-05 12:54:15 -0500

    Refactor simple_interest.sh with comments and author info
    Updated script to include author information and comments for clarity.

--- simple_interest.sh
+++ simple_interest.sh
@@ -1,16 +1,22 @@
-Simple Interest = (Principal x Rate x Time) / 100 
-
-#!/bin/bash
-
-echo "Enter the principal amount:"
-read principal
-
-echo "Enter the annual rate of interest (in %):"
-read rate
-
-echo "Enter the time period (in years):"
-read time
-
-simple_interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
-
-echo "Simple Interest = $simple_interest"
+  #!/bin/bash
+   # This script calculates simple interest given principal,
+   # annual rate of interest and time period in years.
+   # Do not use this in production. Sample purpose only.
+   # Author: Upkar Lidder (IBM)
+   # Additional Authors:
+   # <your GitHub username>
+   # Input:
+   # p, principal amount
+   # t, time period in years
+   # r, annual rate of interest
+   # Output:
+   # simple interest = p*t*r
+   echo "Enter the principal:"
+   read p
+   echo "Enter rate of interest per year:"
+   read r
+   echo "Enter time period in years:"
+   read t
+   s=`expr $p \* $t \* $r / 100`
+   echo "The simple interest is: "
+   echo $s
